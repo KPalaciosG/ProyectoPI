867f880835e5a9b5fed561e0f247a11fca4b845a
kattcon <89096036+kattcon@users.noreply.github.com>
2023-10-08 19:24:01 -0600

    Update tetronimo.asm
    se agregan los demás bloques

--- tetronimo.asm
+++ tetronimo.asm
@@ -6,8 +6,7 @@ section .data
 x: dd 10
 y: dd 20
 
-;matrix: dd 200
-marixLen: dd 200
+marixLen: dd 200 ;tamaño de la matriz
 
 section .text
 global _start
@@ -28,6 +27,41 @@ mov byte[rbx+4], 2
 mov byte[rbx+5], 2
 mov byte[rbx+14], 2
 mov byte[rbx+15], 2
+
+;bloque T:   [3]     color verde
+;         [3][3][3]
+mov byte[rbx+5], 3
+mov byte[rbx+14], 3
+mov byte[rbx+15], 3
+mov byte[rbx+16], 3
+
+;bloque S:   [4][4] color amarillo
+;         [4][4]
+mov byte[rbx+4], 4
+mov byte[rbx+5], 4
+mov byte[rbx+13], 4
+mov byte[rbx+14], 4
+
+;bloque Z:   [5][5]     color rosa
+;               [5][5]
+mov byte[rbx+4], 5
+mov byte[rbx+5], 5
+mov byte[rbx+15], 5
+mov byte[rbx+16], 5
+
+;bloque J: [6]         color morado
+;          [6][6][6]
+mov byte[rbx+4], 6
+mov byte[rbx+14], 6
+mov byte[rbx+15], 6
+mov byte[rbx+16], 6
+
+;bloque L        [7]  color naranja
+;          [7][7][7]
+mov byte[rbx+6], 7
+mov byte[rbx+14], 7
+mov byte[rbx+15], 7
+mov byte[rbx+16], 7
 
 
 ;generacion de bloques
