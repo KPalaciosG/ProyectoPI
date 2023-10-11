c3929ce43438e45a2c727a8c2fd87f3e6d1db35a
kattcon <89096036+kattcon@users.noreply.github.com>
2023-10-11 12:48:08 -0600

    Update tetronimo.asm
    se agrega función moveRight que mueve los tetronimos hacia la derecha

--- tetronimo.asm
+++ tetronimo.asm
@@ -6,7 +6,7 @@ section .data
 x: dd 10
 y: dd 20
 
-marixLen: dd 200 ;tamaño de la matriz
+matrixLen: dd 200 ;tamaño de la matriz
 
 section .text
 global _start
@@ -70,14 +70,36 @@ getBlock:
 	cmp sil, 0
 	je increase
 
-	mov rax, rsi
+	mov rax, rsi ;muevo a rax el tipo de bloque (1:I, 2:O, 3:T etc)
 	jmp end
 
 	increase: 
 	inc rbx
 	loop getBlock
-;---------- end getBlock----------
+;---------- end getBlock-----------
 
+;-----------moveRight---------------
+;mover hacia la derecha
+;+1 para mover hacia la derecha, se empieza desde abajo de la matriz hacia arriba
+	
+moveRight:
+	
+	mov ecx, 200
+	mov rbx, matrix
+
+	startMoveRight:
+	cmp byte[rbx+rcx],0
+	je loopRight
+
+	mov dl, byte[rbx+rcx]
+	mov byte[rbx+rcx],0
+	mov byte[rbx+rcx+1], dl
+	
+	loopRight: 
+
+	loop startMoveRight	
+
+;-----------end moveRight-------------
 
 ;generacion de bloques
 
